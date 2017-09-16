b0ee76961297962fe68b5762ac67f6ffe68229b3
Vasiliy Shubko <flit77@gmail.com>
2017-09-17 01:45:06 +0200

    non-declaration error fixed

--- main.go
+++ main.go
@@ -20,12 +20,13 @@ type Address struct {
 
 var people []Person
 
-people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
-people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
-people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
-
 // our main function
 func main() {
+
+    people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
+    people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
+    people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
+
     router := mux.NewRouter()
     router.HandleFunc("/people", GetPeople).Methods("GET")
     router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
