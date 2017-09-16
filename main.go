be33548ee8adafdd88e1f93e01abd005238517c3
Vasiliy Shubko <flit77@gmail.com>
2017-09-17 00:29:08 +0200

    handler functions added

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "encoding/json"
+    // "encoding/json"
     "log"
     "net/http"
     "github.com/gorilla/mux"
@@ -10,5 +10,14 @@ import (
 // our main function
 func main() {
     router := mux.NewRouter()
+    router.HandleFunc("/people", GetPeople).Methods("GET")
+    router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
+    router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
+    router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
     log.Fatal(http.ListenAndServe(":8000", router))
 }
+
+func GetPeople(w http.ResponseWriter, r *http.Request) {}
+func GetPerson(w http.ResponseWriter, r *http.Request) {}
+func CreatePerson(w http.ResponseWriter, r *http.Request) {}
+func DeletePerson(w http.ResponseWriter, r *http.Request) {}
